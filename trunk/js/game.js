f1c1c7bf1185aada2ce0a779441544da65f6f517
phedrik1 <phedrik1@49eb1776-f068-11dd-aa46-671127399dce>
2009-02-01 15:36:58 +0000

    Modif de la methode load_ferme et ajout action gateway

--- trunk/js/game.js
+++ trunk/js/game.js
@@ -1,8 +1,16 @@
 var Game = function () {
 	var obj = this;
+			//AJAX RECUPT X ET Y DE LA FERME
 	
 	obj.load_ferme = function() {
-		$('ferme_loading').style.visibility='visible';
+		//recuperer et creer la ferme.
+		req = new Ajax();
+		req.file = 'gateway.php?action=data_ferme';
+		req.onsucess = function (callback) {
+			$('ferme_display').innerHTML = callback;
+		}
+		req.start();
 		
 	}
+	
 }
