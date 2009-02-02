312c7c7fbdaa621752786fa7f32af65b69980bc9
phedrik1 <phedrik1@49eb1776-f068-11dd-aa46-671127399dce>
2009-02-02 17:24:07 +0000

    Ajout des images de test + corection bug affichage de la map

--- gateway.php
+++ gateway.php
@@ -4,15 +4,29 @@ include('include/utils.php');
 if(!empty($_GET['action'])) {
   switch($_GET['action']) {
     case 'data_ferme' :
-      echo'<table>';
-      for($i=0;$i<10;$i++) {
+    $x = 10;
+    $y = 10;
+    $bat_test = array(3,3);
+    
+      echo'<div id="ferme"><table>';
+      for($i=0;$i<$y;$i++) {
         echo'<tr>';
-        for($j=0;$j<10;$j++) {
-          echo'<td class="case"></td>';
+        for($j=0;$j<$x;$j++) {
+          if($i == $y-1 OR $i == 0) 
+            echo'<td background="img/herbe_tb.png"></td>';
+          elseif($j == 0) 
+            echo'<td background="img/herbe_l.png"></td>';
+          elseif($j == $x-1) 
+            echo'<td background="img/herbe_r.png"></td>';
+          elseif($bat_test[0] == $j AND $bat_test[1] == $i) 
+            echo'<td background="img/bat.png"></td>';
+          else
+            echo'<td background="img/herbe.png"></td>';
+          
         }
         echo'</tr>';
       }
-      echo'</table>';
+      echo'</table></div>';
     break;
   
     default:
